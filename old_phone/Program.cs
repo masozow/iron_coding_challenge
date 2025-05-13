bd5170edfcf66d47810dca01eae38341f4cffe2a
masozow <14838605+masozow@users.noreply.github.com>
2025-05-13 10:04:06 -0600

    adding a validation for the written input

--- old_phone/Program.cs
+++ old_phone/Program.cs
@@ -5,12 +5,13 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Write your message, always ending with a #:");
-        string message = Console.ReadLine() ?? "";
+        string writtenMessage = Console.ReadLine() ?? "";
+        string message = writtenMessage.Contains('#') ? DecipheredMessage.OldPhonePad(Validation.ValidateMessage, writtenMessage) : "Invalid input, you should write a # after the digits.";
         // string[] messages=["33#","227*#","4433555 555666#","8 88777444666*664#"];
         // foreach(string message in messages){
         //     Console.WriteLine("Your message is: " + DecipheredMessage.OldPhonePad(Validation.ValidateMessage, message));
         //     Console.WriteLine("-----------------");
         // }
-        Console.WriteLine("Your message is: " + DecipheredMessage.OldPhonePad(Validation.ValidateMessage, message));
+        Console.WriteLine("Your message is: " + message);
     }
 }
